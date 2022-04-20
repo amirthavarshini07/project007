f26b28c8a44b42693687f8741f402630722e25d0
amirthavarshini07 <amirtharaman07@gmail.com>
2022-04-20 19:54:40 +0530

    varsha's code

--- src/test/java/org/test/Sample.java
+++ src/test/java/org/test/Sample.java
@@ -5,8 +5,8 @@ import org.openqa.selenium.support.FindBy;
 
 public class Sample{
 	public static void main(String[] args) {
-		System.out.println("test3");
-		System.out.println("test4");
+		System.out.println("test7");
+		System.out.println("test6");
 
 }
 }
