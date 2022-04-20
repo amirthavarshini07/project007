99a2b454f16298da6bfd1e0cb824ecadd6508193
amirthavarshini07 <amirtharaman07@gmail.com>
2022-04-20 12:52:30 +0530

    selenium8

---
+++ src/test/java/org/test/Sample.java
@@ -0,0 +1,12 @@
+package org.test;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Sample{
+	public static void main(String[] args) {
+		System.out.println("test3");
+		System.out.println("test4");
+
+}
+}
